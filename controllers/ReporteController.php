078683c8b8a8eb624317d021ec695b0390d858ff
Pamela Pricsila Bravo Medina <165944643+PricsilaM@users.noreply.github.com>
2025-10-08 00:58:20 -0500

    Add use statement for ReporteModel in ReporteController

--- controllers/ReporteController.php
+++ controllers/ReporteController.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../models/PagoModel.php';
 require_once __DIR__ . '/../models/EstudianteModel.php';
 
 use Core\BaseController;
+
 use Models\ReporteModel;
 use Models\PagoModel;
 use Models\EstudianteModel;
